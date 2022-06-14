0d44972cd994c727482847a85e41ad3d1eae0845
ha84yuki <c0a21104a4@edu.teu.ac.jp>
2022-06-14 16:09:58 +0900

    修正 #1

--- ex01/quiz.py
+++ ex01/quiz.py
@@ -1,20 +1,20 @@
 def Q1():
     A1 = input("問題：\nサザエの旦那の名前は？\n答えるんだ:")
-    if A1 == "マスオ" and "ますお":
+    if A1 == "マスオ" or "ますお":
         print("正解!!!")
     else:
         print("出直してこい")
 
 def Q2():
     A2 = input("問題：\nカツオの妹の名前は？\n答えるんだ:")
-    if A2 == "わかめ" and "ワカメ":
+    if A2 == "わかめ" or "ワカメ":
         print("正解!!!")
     else:
         print("出直してこい")
 
 def Q3():
     A3 = input("問題：\nタラオはカツオから見てどんな関係？\n答えるんだ:")
-    if A3 == "甥" and "おい" and "甥っ子" and "おいっこ":
+    if A3 == "甥" or "おい" or "甥っ子" or "おいっこ":
         print("正解!!!")
     else:
         print("出直してこい")
