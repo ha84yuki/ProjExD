cc0c4a9d6f425a35350700f845fafc0de272e731
ha84yuki <c0a21104a4@edu.teu.ac.jp>
2022-06-21 14:26:09 +0900

    2

--- ex02/calc.py
+++ ex02/calc.py
@@ -4,4 +4,14 @@ if __name__ == '__main__':
     root = tk.Tk()
     root.geometry("300x500")
     root.title("電卓")
+
+    r,c=0,0
+    for i in range(9,-1,-1):
+        btn =tk.Button(root,text=i,width=4, height=2,font=("Times New Roman",30))
+        btn.grid(row=r,column=c)
+        c+=1
+        if (i-1)%3==0:
+            r+=1
+            c=0
+            
     root.mainloop()
