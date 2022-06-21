0cb58626c6ac4fae1e19a09ee611a38eb33262a2
ha84yuki <c0a21104a4@edu.teu.ac.jp>
2022-06-21 16:56:51 +0900

    完成

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,3 +1,4 @@
+from random import randint, random
 import tkinter as tk
 import tkinter.messagebox as tkm
 
@@ -13,6 +14,42 @@ def click_eqall(event):#=の関数
     entry.delete(0,tk.END)
     entry.insert(tk.END,str(res))
 
+def click_alclear(event):#文字列全削除
+    entry.delete(0,tk.END)
+    entry.insert(tk.END)
+    
+def click_clear(event):#一文字削除
+    cl=entry.get()
+    cl2=cl[:-1]
+    entry.delete(0,tk.END)
+    entry.insert(tk.END,str(cl2))
+
+def click_lucky(event):
+    entry.delete(0,tk.END)
+    color_n=randint(1,7)
+    if color_n==1:
+        color="赤"
+        entry["bg"] = "#ff0000"
+    elif color_n==2:
+        color="橙"
+        entry["bg"] = "#ffa000"
+    elif color_n==3:
+        color="黄"
+        entry["bg"] = "#ffff00"
+    elif color_n==4:
+        color="緑"
+        entry["bg"] = "#008000"
+    elif color_n==5:
+        color="青"
+        entry["bg"] = "#0000ff"
+    elif color_n==6:
+        color="藍"
+        entry["bg"] = "#2e4b71"
+    else:
+        color="紫"
+        entry["bg"] = "#800080"
+    entry.insert(tk.END,str(color))
+
 if __name__ == '__main__':
     root = tk.Tk()
     #root.geometry("300x600")
@@ -22,7 +59,7 @@ if __name__ == '__main__':
     entry.grid(row=0,column=0,columnspan=3)
 
     r,c=1,0
-    for i,num in enumerate([9,8,7,6,5,4,3,2,1,0,"+","="]):
+    for i,num in enumerate(["AC","C","/","*",9,8,7,"-",6,5,4,"+",3,2,1,"=",0,"LC"]):
         btn =tk.Button(root,
                        text=num,
                        width=4,
@@ -34,7 +71,13 @@ if __name__ == '__main__':
         c+=1
         if num=="=":
             btn.bind("<1>",click_eqall)
-        if (i+1)%3==0:
+        if num=="AC":
+            btn.bind("<1>",click_alclear)
+        if num=="C":
+            btn.bind("<1>",click_clear)
+        if num=="LC":
+            btn.bind("<1>",click_lucky)
+        if (i+1)%4==0:
             r+=1
             c=0
 
