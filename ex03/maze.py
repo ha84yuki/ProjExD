26e846178e6cddd76309bf63e6289b7cfbac9149
ha84yuki <c0a21104a4@edu.teu.ac.jp>
2022-06-28 15:35:14 +0900

    11,12

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,25 +1,26 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
+from unittest import skip
 import maze_maker as mk
 
 def key_down(event):
     global key 
     key=event.keysym
-
 def key_up(event):
     global key
     key=""
 
-def main_proc():
-    global cx,cy,key
-    if key == "Up":
-        cy-=20
-    elif key=="Down":
-        cy+=20
-    elif key=="Right":
-        cx+=20
-    elif key=="Left":
-        cx-=20
+def main_proc():#こうかとんの移動
+    global cx,cy,key,mx,my
+    if key == "Up" and maze_bg[my-1][mx]==0:
+            my-=1
+    elif key=="Down" and maze_bg[my+1][mx]==0:
+        my+=1
+    elif key=="Right" and maze_bg[my][mx+1]==0:
+        mx+=1
+    elif key=="Left" and maze_bg[my][mx-1]==0:
+        mx-=1
+    cx,cy=100*mx+50,100*my+50
 
     canvas.coords("tori",cx,cy)
     root.after(100,main_proc)
@@ -31,16 +32,17 @@ if __name__ == "__main__":
     canvas=tk.Canvas(root,width=1500,height=900,background='black')#サイズと背景色
     canvas.pack()
 
-    maze_bg=mk.make_maze(15,9)
+    maze_bg=mk.make_maze(15,9)#背景の描画
     mk.show_maze(canvas,maze_bg)
 
     tori= tk.PhotoImage(file='fig/2.png')#こうかとん配置
-    cx,cy=300,400
+    mx,my=1,1
+    cx,cy=100*mx+50,100*my+50
     canvas.create_image(cx,cy,image=tori,tag='tori')
-    
+
     key= ""
     root.bind("<KeyPress>",key_down)
     root.bind("<KeyRelease>",key_up)
-
+    
     root.after(100,main_proc)
     root.mainloop()
