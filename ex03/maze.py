5f21b8698fdabca74aa294a16e59d549f87fbb75
ha84yuki <c0a21104a4@edu.teu.ac.jp>
2022-06-28 14:16:36 +0900

    1,2

---
+++ ex03/maze.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+import tkinter.messagebox as tkm
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title('迷えるこうかとん')
+    
+    canvas=tk.Canvas(root,width=1500,height=900,background='black')#サイズと背景色
+    canvas.pack()
+
+    root.mainloop()
