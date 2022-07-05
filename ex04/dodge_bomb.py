3db1805c058830ae98c8164dba1dace9d732ca4c
ha84yuki <c0a21104a4@edu.teu.ac.jp>
2022-07-05 16:51:14 +0900

    ４限追加機能実装完了

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,6 +1,12 @@
 import pygame as pg
 import sys
 import random
+import tkinter as tk
+import tkinter.messagebox as tkm
+import datetime
+
+st=datetime.datetime.now()
+
 def main():
     clock=pg.time.Clock()
     #練習１
@@ -12,7 +18,8 @@ def main():
     screen_sfc.blit(bgimg_sfc,bgimg_rct)             #背景画像用Surfaceを画面用Surfaceに貼り付ける
 
     #練習３
-    kkimg_sfc=pg.image.load("fig/6.png")             #こうかとん画像用のSurface
+    num= random.randint(0,9)
+    kkimg_sfc=pg.image.load(f"fig/{num}.png")             #こうかとん画像用のSurface
     kkimg_sfc= pg.transform.rotozoom(kkimg_sfc,0,2.0)
     kkimg_rct=kkimg_sfc.get_rect()                   #こうかとん画像用のRect
     kkimg_rct.center=900,400                        #こうかとん画像の中心座標を設定する
@@ -20,13 +27,16 @@ def main():
 
     #練習５
     bmimg_sfc = pg.Surface((20,20))   #Surface
-    bmimg_sfc.set_colorkey((0,0,0,))#爆弾用のStrface
+    bmimg_sfc.set_colorkey((0,0,0,))#爆弾用のSurface
     pg.draw.circle(bmimg_sfc, (255, 0, 0), (10,10), 10)  #爆弾用のSurfaceに円を描く
     bmimg_rct=bmimg_sfc.get_rect()                       #爆弾用Rect
+    bmimg_rct2=bmimg_sfc.get_rect()   
     bmimg_rct.centerx = random.randint(0, screen_rct.width)
     bmimg_rct.centery = random.randint(0, screen_rct.height)
-    screen_sfc.blit(bmimg_sfc,bmimg_rct)                     #爆弾用のSurfaceを画面用Surfaceに貼り付ける
+    bmimg_rct2.centerx = random.randint(0, screen_rct.width)
+    bmimg_rct2.centery = random.randint(0, screen_rct.height) 
     vx,vy= +1,+1
+    vxx,vyy=+1,+1
     
     while True:
         screen_sfc.blit(bgimg_sfc,bgimg_rct)               #背景画像用Surfaceを画面用Surfaceに貼り付ける
@@ -50,6 +60,9 @@ def main():
 
         #練習６
         bmimg_rct.move_ip(vx,vy)
+        bmimg_rct2.move_ip(vxx,vyy)
+        screen_sfc.blit(bmimg_sfc,bmimg_rct)                     #爆弾用のSurfaceを画面用Surfaceに貼り付ける
+        screen_sfc.blit(bmimg_sfc,bmimg_rct2)
 
         #練習５
         screen_sfc.blit(bmimg_sfc,bmimg_rct)
@@ -58,10 +71,23 @@ def main():
         x,y = check_bound(bmimg_rct,screen_rct)
         vx*=x
         vy*=y
+        x,y = check_bound(bmimg_rct2,screen_rct)
+        vxx*=x
+        vyy*=y
+
+        ed= datetime.datetime.now()
+        tmr = (ed-st).seconds
+
+        fonto = pg.font.Font(None,80)
+        txt = fonto.render(str(tmr), True,(0,0,0))
+        screen_sfc.blit(txt,(100,100))
 
         #練習８
-        if kkimg_rct.colliderect(bmimg_rct): return
-
+        if kkimg_rct.colliderect(bmimg_rct) or kkimg_rct.colliderect(bmimg_rct2): 
+            kkimg_sfc=pg.image.load("fig/bakuhatsu.png")
+            screen_sfc.blit(kkimg_sfc, kkimg_rct)
+            tkm.showinfo(root,f"Game over \n{tmr}秒生きました")
+            return
 
         pg.display.update()                           #画面の更新
         clock.tick(1000)
@@ -74,6 +100,7 @@ def check_bound(rct,scr_rct):#画面内：+1 / 画面外：-1
     return x,y
     
 if __name__=="__main__":
+    root =tk.Tk()
     pg.init()
     main()
     pg.quit()
