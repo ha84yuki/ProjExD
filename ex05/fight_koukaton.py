9815b13d81707d249b71d70c94496a6afaf4ab81
ha84yuki <c0a21104a4@edu.teu.ac.jp>
2022-07-12 17:14:00 +0900

    4限追加機能実装完了

--- ex05/fight_koukaton.py
+++ ex05/fight_koukaton.py
@@ -1,6 +1,8 @@
 import pygame as pg
 import sys
 import random
+STATE=False
+bg=True
 
 class Screen:
     def __init__(self,title,wh,image):
@@ -72,13 +74,37 @@ class Bomb:
         # 練習5
         self.blit(scr)          
 
+class CUONTDOWN():
+    def __init__(self,TMP) :
+        #カウントダウンシステム
+        self.TMS=TMP*100
+        self.TMR=TMP
+        self.fonto=pg.font.Font(None,80)
+        self.txt=self.fonto.render('{:.0f}'.format(self.TMR),True,(0,0,0))
+        
+        
+    def update(self):
+        self.fonto=pg.font.Font(None,80)
+        self.txt=self.fonto.render('{:.0f}'.format(self.TMR),True,(0,0,0))
+        self.TMR -=0.01
+        self.TMS -=1
+        
+        
+def bgm():
+    # 音楽ファイルの読み込み
+    pg.mixer.music.load("./fig/Floor_Beast.mp3") 
+    pg.mixer.music.play(loops=-1, start=0.0)#ロードした音楽の再生
+
 
 def main():
     global STATE,G_O
     clock = pg.time.Clock()
     scr = Screen("逃げろ！こうかとん", (1600, 900), "fig/pg_bg.jpg")
     kkt = Bird("fig/6.png", 2.0, (900, 400))
-    bkd = Bomb((255,0,0), 10, (+1,+1), scr)
+    bkd= Bomb((255,0,0), 10, (+1,+1), scr)
+    bkd2 = Bomb((0,255,0), 37, (+2,+4), scr)
+    bkd3 = Bomb((0,0,255), 17, (+3,+3), scr)
+    cd=CUONTDOWN(15)#残り時間
 
     while True:
         scr.blit()
@@ -89,7 +115,16 @@ def main():
 
         kkt.update(scr)
         bkd.update(scr)
-        if kkt.rct.colliderect(bkd.rct):
+        bkd2.update(scr)
+        bkd3.update(scr)
+        if kkt.rct.colliderect(bkd.rct) or kkt.rct.colliderect(bkd2.rct) or kkt.rct.colliderect(bkd3.rct):
+            return
+
+        scr.sfc.blit(cd.txt,(200,200))
+        cd.update()#タイムの更新
+        cl=tmr_c(cd.TMS)#残り0秒か判定
+        if cl ==True:
+            pg.mixer.music.stop()
             return
 
         pg.display.update()
@@ -107,9 +142,34 @@ def check_bound(rct, scr_rct):
     if rct.top  < scr_rct.top  or scr_rct.bottom < rct.bottom: tate = -1 # 領域外
     return yoko, tate
 
+def tmr_c(tms):#のこり0秒か判定
+    global STATE
+    if tms ==0:
+        STATE=True
+        return True
+
+def crear():#Gamecrear処理
+    pg.display.set_caption("逃げろ！こうかとん")
+    screen=pg.display.set_mode((960,204))
+    pg.mixer.music.load("./fig/Filter_Luv.mp3") 
+    pg.mixer.music.play(loops=-1, start=0.0)#ロードした音楽の再生
+    while True:
+        for event in pg.event.get():
+            if event.type==pg.QUIT: return
+        g_img=pg.image.load("fig/text_gameclear_j.png")             #クリア画像用のSurface
+        g_img= pg.transform.rotozoom(g_img,0,0.3)
+        g_rect=g_img.get_rect()                   #クリア画像用のRect
+        g_rect.center=480,102                      #クリア画像の中心座標を設定する
+        screen.blit(g_img,g_rect) 
+        pg.display.update()
+
 
 if __name__ == "__main__":
     pg.init()
+    bgm()
     main()
+    print(STATE)
+    if STATE ==True:#カウントダウンが0ならば
+        crear()
     pg.quit()
     sys.exit()
